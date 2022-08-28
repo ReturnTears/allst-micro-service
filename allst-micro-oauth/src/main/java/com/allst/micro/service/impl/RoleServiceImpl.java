58deab7ad3e670fac732583bd5dd97982bd3d9bd
returntears <returntears@outlook.com>
2022-08-28 22:54:05 +0800

    oauth-添加@Qualifier注解注释

--- allst-micro-oauth/src/main/java/com/allst/micro/service/impl/RoleServiceImpl.java
+++ allst-micro-oauth/src/main/java/com/allst/micro/service/impl/RoleServiceImpl.java
@@ -18,7 +18,10 @@ import java.util.Set;
 @Service
 public class RoleServiceImpl implements IRoleService {
 
-
+    /**
+     * 通过将 @Qualifier 注解与我们想要使用的特定 Spring bean 的名称一起进行装配，
+     * Spring 框架就能从多个相同类型并满足装配要求的 bean 中找到我们想要的，避免让Spring脑裂。
+     */
     @Qualifier("allst-micro-boot")
     @Autowired
     OrganizationProvider organizationProvider;
