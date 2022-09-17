fe86e3e9e52e25fcf8547c8f9a0ff8ea612d4dba
returntears <returntears@outlook.com>
2022-09-17 22:24:35 +0800

    Boot模块中调用User模块的服务demo

--- allst-micro-boot/src/main/java/com/allst/micro/controller/UserController.java
+++ allst-micro-boot/src/main/java/com/allst/micro/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.allst.micro.controller;
 
+import com.allst.micro.dto.UserDTO;
+import com.allst.micro.remote.UserRemoteService;
+import com.allst.micro.response.ResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,12 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Hutu
  * @since 2022-08-27 下午 09:19
  */
-/*@RestController
-@RequestMapping("/user")*/
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
-    /*@Autowired
-    UserRemoteService remoteService;*/
+    @Autowired
+    UserRemoteService remoteService;
 
+    @GetMapping("/getUserById")
+    public ResponseDTO<?> getUserById(@RequestParam("userId") Integer userId) {
+        UserDTO userDTO = remoteService.getUserById(userId);
+
+        return ResponseDTO.success(userDTO);
+    }
     
 }
